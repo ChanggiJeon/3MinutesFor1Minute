e6cbed8bdcd4aed00ce032497fa2dd19ee750df1
윤재성 <wotjd4725@gmail.com>
2022-03-09 15:57:51 +0900

    Feat/BE/Add: frame of signup and login & swagger base

--- BE/api/accounts/models.py
+++ BE/api/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    profile_image=models.ImageField(blank=True, upload_to='images/')
+    creatd_at=models.DateTimeField(auto_now_add=True)
