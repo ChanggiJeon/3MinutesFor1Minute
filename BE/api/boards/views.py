ad8b1e35c5b78b341f7514629b85068f89bf8266
kiel <aceg1541@naver.com>
2022-03-09 19:48:34 +0900

    Feat/BE/Add : boards & comments CRUD

--- BE/api/boards/views.py
+++ BE/api/boards/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Board, BoardComment
+from .serializers import BoardListSerializer, BoardSerializer, BoardCommentSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def board_list(request):
+    if request.method == 'GET':
+        boards = get_list_or_404(Board)
+        serializer = BoardListSerializer(boards, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BoardSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def board_detail(request, board_pk):
+    board = get_object_or_404(Board, pk=board_pk)
+
+    if request.method == 'GET':
+        serializer = BoardSerializer(board)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        board.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = BoardSerializer(board, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+@api_view(['POST', 'DELETE', 'PUT'])
+def board_comment(request, board_pk, comment_pk=None):
+    if request.method == 'POST':
+        board = get_object_or_404(Board, pk=board_pk)
+        serializer = BoardCommentSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(board=board)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    elif request.method == 'DELETE':
+        comment = get_object_or_404(BoardComment, pk=comment_pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        comment = get_object_or_404(BoardComment, pk=comment_pk)
+        serializer = BoardCommentSerializer(comment, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
