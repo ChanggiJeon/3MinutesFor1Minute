7b49b0154e04000939395d243e77aa808496e6de
kiel <aceg1541@naver.com>
2022-03-09 19:48:34 +0900

    Feat/BE/Add : boards & comments CRUD

---
+++ BE/api/boards/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Board, BoardComment
+
+
+class BoardCommentSerializer(serializers.ModelSerializer):
+    comments_filter = serializers.SerializerMethodField('filt')
+
+    def filt(self, comment):
+        comments = BoardComment.objects.filter(parent=comment)
+        serializer = BoardCommentSerializer(comments, many=True)
+        return serializer.data
+
+    class Meta:
+        model = BoardComment
+        fields = '__all__'
+        read_only_fields = ('board', )
+
+
+class BoardListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Board
+        fields = ('id', 'title', )
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    comments_filter = serializers.SerializerMethodField('filt')
+
+    def filt(self, board):
+        comments = BoardComment.objects.filter(board=board)
+        serializer = BoardCommentSerializer(comments, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Board
+        fields = '__all__'
