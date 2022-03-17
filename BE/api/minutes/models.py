abe0f74179bbbb14a121fd03285279bff7270530
kiel <aceg1541@naver.com>
2022-03-17 21:34:42 +0900

    Feat/BE/Add : speech but no test

--- BE/api/minutes/models.py
+++ BE/api/minutes/models.py
@@ -1,24 +1,44 @@
 from django.db import models
-from django.conf import settings
+from ..community import models
 
 
 class Minute(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=500)
-    summary = models.TextField(null=True)
-    keyword = models.TextField(null=True)
-    record_file = models.FileField(upload_to='', null=True)
-    text_file = models.FileField(upload_to='', null=True)
+    content = models.TextField(max_length=300)
+    conclusion = models.TextField(max_length=300)
+    is_closed = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    reference_file = models.FileField(upload_to='', null=True)
 
     def __str__(self):
         return self.title
 
 
-class MinuteComment(models.Model):
+class Participant(models.Model):
     minute = models.ForeignKey(Minute, on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    is_assignee = models.BooleanField(default=False)
+
+
+class Speech(models.Model):
+    minute = models.ForeignKey(Minute, on_delete=models.CASCADE)
+    participant = models.OneToOneField(Participant, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+    summary = models.TextField(max_length=400)
+    cloud_keyword = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    record_file = models.FileField(upload_to='', null=True)
+    reference_file = models.FileField(upload_to='', null=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class SpeechComment(models.Model):
+    speech = models.ForeignKey(Speech, on_delete=models.CASCADE)
     content = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
