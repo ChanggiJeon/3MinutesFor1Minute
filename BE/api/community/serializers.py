47adb47b2cd858f0121e0106174e054d1248438d
윤재성 <wotjd4725@gmail.com>
2022-03-17 16:37:26 +0900

    Feat/BE/Add: community code and name check

--- BE/api/community/serializers.py
+++ BE/api/community/serializers.py
@@ -1,5 +1,7 @@
 from calendar import c
 from rest_framework import serializers
+
+import community
 from .models import Community, Member
 
 class CommunitySerializer(serializers.ModelSerializer):
@@ -7,6 +9,11 @@ class CommunitySerializer(serializers.ModelSerializer):
         model = Community
         fields = '__all__'
 
+class CommunitySearchSerializer(serializers.ModelSerializer):
+    is_joined = serializers.BooleanField(read_only=True)
+    class Meta:
+        model = Community
+        fields = ('id', 'name','is_joined')
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
