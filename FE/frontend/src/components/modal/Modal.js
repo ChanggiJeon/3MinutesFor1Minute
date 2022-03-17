9317477786b8b7f307780a6302db0c5b65135b57
ruokic <gusqo327@naver.com>
2022-03-17 13:42:08 +0900

    Feat/FE/Add : login api in accounts

--- FE/frontend/src/components/modal/Modal.js
+++ FE/frontend/src/components/modal/Modal.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FiX } from 'react-icons/fi';
 
 const Container = styled.div`
 	position: absolute;
@@ -16,6 +17,17 @@ const Wrapper = styled.div`
 	background-color: ${props => props.theme.backgroundColor};
 	padding: 20px;
 	border-radius: 5px;
+	position: relative;
+`;
+
+const ExitBtn = styled.button`
+	position: absolute;
+	right: 10px;
+	top: 10px;
+	border: none;
+	font-size: 30px;
+	background-color: inherit;
+	cursor: pointer;
 `;
 
 function Modal({ children, setMode }) {
@@ -25,9 +37,18 @@ function Modal({ children, setMode }) {
 		}
 	};
 
+	const handleModeBtn = e => {
+		setMode(false);
+	};
+
 	return (
 		<Container onClick={handleMode}>
-			<Wrapper>{children}</Wrapper>
+			<Wrapper>
+				<ExitBtn onClick={handleModeBtn}>
+					<FiX />
+				</ExitBtn>
+				{children}
+			</Wrapper>
 		</Container>
 	);
 }
