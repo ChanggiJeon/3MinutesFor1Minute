2c5673c35888aa4ebd9f4d0a120cedb3f5e1841e
John Kim <dhkdhd123@naver.com>
2022-03-21 23:58:51 +0900

    Feat/FE/Add : mainpage UI

--- FE/frontend/src/styles/GlobalStyles.js
+++ FE/frontend/src/styles/GlobalStyles.js
@@ -17,6 +17,8 @@ const GlobalStyles = createGlobalStyle`
 		font-family: 'HoengseongHanu';
 	}
 
+
+
 	@font-face {
 		font-family: 'EliceDigitalBaeum-Bd';
 		src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_elice@1.0/EliceDigitalBaeum-Bd.woff2') format('woff2');
