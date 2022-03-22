adbc8fcb8271c28d3c89ded8201c1952139ea685
ruokic <gusqo327@naver.com>
2022-03-22 11:22:33 +0900

    Feat/FE/Add : community creation modal

--- FE/frontend/src/views/Main.js
+++ FE/frontend/src/views/Main.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import CreateCommunity from '../components/main/CreateCommunity';
+import Modal from '../components/modal/Modal';
 
 const Test = styled.div`
-  background-color: ${props => props.theme.backgroundColor};
-  color: ${props => props.theme.fontColor};
+	background-color: ${props => props.theme.backgroundColor};
+	color: ${props => props.theme.fontColor};
 `;
 
-function Main () {
-  return <Test>Hello~</Test>
+function Main() {
+	const [isCreateMode, setCreateMode] = useState(true);
+
+	const CreateCommunityModal = isCreateMode && (
+		<Modal setMode={setCreateMode}>
+			<CreateCommunity />
+		</Modal>
+	);
+
+	return <Test>Hello~{CreateCommunityModal}</Test>;
 }
 
-export default Main
+export default Main;
