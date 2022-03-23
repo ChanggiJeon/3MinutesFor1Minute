aff39af01fb876d7c2ad24268e5ccda47d09a746
John Kim <dhkdhd123@naver.com>
2022-03-23 15:14:32 +0900

    Feature/FE/Add : community mainpage UI
    sidebar & post boxes

---
+++ FE/frontend/src/components/community/main/SubBox.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const SubBox = styled.div`
+  width: 30%;
+  height: 40%;
+  background-color: #FFFFFF;
+  border-radius: 50px;
+  padding: 10px;
+  margin-top: 20px;
+`
+
+export default SubBox;
