c3fb38ab309b21df59cde498d1c2fd38b162b61c
kiel <aceg1541@naver.com>
2022-03-24 15:48:28 +0900

    Feat/BE/Add accounts swagger

--- BE/api/minutes/admin.py
+++ BE/api/minutes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Minute, Participant, Speech, SpeechComment
 
-# Register your models here.
+
+admin.site.register(Minute)
+admin.site.register(Participant)
+admin.site.register(Speech)
+admin.site.register(SpeechComment)
