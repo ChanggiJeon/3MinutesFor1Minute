a2663b66e0b1c36bcbda5376e38aab95267d70ae
kiel <aceg1541@naver.com>
2022-03-24 15:48:28 +0900

    Feat/BE/Add accounts swagger

--- BE/api/accounts/serializers.py
+++ BE/api/accounts/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = ('id','username', 'password', 'email', 'name')
+        fields = ('id', 'username', 'password', 'email', 'name', )
+        read_only_fields = ('id', )
+        write_only_fields = ('password', )
--- BE/api/accounts/urls.py
+++ BE/api/accounts/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
-# 프로필 이미지 전용
-from django.conf import settings
-from django.conf.urls.static import static
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-app_name = "accounts"
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('signup/', views.signup),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('delete/', views.delete),
-    path('uniquecheck/username/', views.unique_check_username),
-    path('uniquecheck/email/', views.unique_check_email),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('uniquecheck/username/<username>/', views.unique_check_username),
+    path('uniquecheck/email/<email>/', views.unique_check_email),
+]
