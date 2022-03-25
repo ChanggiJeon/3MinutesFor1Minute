7be12330a5e0d6ea537984ff6ca2a5a289868e22
전창기 <wjs1724@naver.com>
2022-03-25 11:41:35 +0900

    Docs : Add some pictures, and model.pt
    Former-commit-id: bd58878470572bcece05420966d07bcf2227d54d

--- BE/AI/Summarize/summarize.py
+++ BE/AI/Summarize/summarize.py
@@ -5,7 +5,7 @@ from transformers import BartForConditionalGeneration
 tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
 model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
 
-async def summery(text : str):
+def summery(text : str):
     raw_input_ids = tokenizer.encode(text)
     input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
     summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
