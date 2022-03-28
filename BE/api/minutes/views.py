6b60508b61d73db3e63a6c0f259c84b1fa291dba
전창기 <wjs1724@naver.com>
2022-03-28 14:59:58 +0900

    Feat/BE/Add : textrank function in summarize, fix views.py in minutes
    and change folder name summarize to summarization
    
    
    Former-commit-id: 5e3217bfabd970a8987df28b1944c7c9ca0ec2b4

--- BE/api/minutes/views.py
+++ BE/api/minutes/views.py
@@ -18,7 +18,7 @@ from community.serializers import MemberSerializer
 import sys
 sys.path.append('.')
 from AI.STT.API.google import upload_file, transcribe_gcs
-from AI.Summarize.summarize import summery
+from AI.Summarization.summarize import summery, summarize
 from AI.Wordslist.wordslist import wordslist
 from config.settings import MEDIA_ROOT
 
@@ -27,8 +27,9 @@ def AI(file_path, file_name):
     upload_file(file_path, file_name)
     text = transcribe_gcs(file_name)
     summary = summery(text)
+    summarization = summarize(text, ratio=0.4)
     cload_keyword = wordslist(text)
-    return text, summary, cload_keyword
+    return text, summary, cload_keyword, summarization
 
 
 @api_view(['GET'])
