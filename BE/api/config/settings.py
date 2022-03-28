a298f72cd068a6b46e46435b25f068c4a64b2610
윤재성 <wotjd4725@gmail.com>
2022-03-28 13:38:59 +0900

    Chore: SSL Test 1#
    Former-commit-id: d0850b2320e7cc224db6490ffa48ae2d92dea8cd

--- BE/api/config/settings.py
+++ BE/api/config/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+ALLOWED_HOSTS = ['127.0.0.1']
 CORS_ORIGIN_WHITELIST = env.list('ALLOWED_HOSTS')
 CORS_ALLOW_CREDENTIALS = True
 
