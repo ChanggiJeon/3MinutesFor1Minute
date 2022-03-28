454cbcaf470e3db5cef11c512259ec532e93c4cc
kiel <aceg1541@naver.com>
2022-03-28 17:44:04 +0900

    Feat/BE/Change MemberSerializer by id to nickname
    Former-commit-id: 318a98c552ec0eb551d09a51c548d7aa2a232f7c

--- BE/api/accounts/models.py
+++ BE/api/accounts/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    profile_image = models.ImageField(upload_to='images/', blank=True)
+    profile_image = models.ImageField(upload_to='images/', null=True)
     creatd_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=16, blank=True)
+
+    def __str__(self):
+        return self.name
