bc92c036ac7eca2389d8180a934034edf95c8a0b
John Kim <dhkdhd123@naver.com>
2022-03-29 16:03:10 +0900

    Feat/FE/add : create minutes page UI
    Former-commit-id: eff3d0c49df8d6141945e4d02cbcc00e8c0dae55

--- FE/frontend/src/components/community/minutes/list/MinutesItem.js
+++ FE/frontend/src/components/community/minutes/list/MinutesItem.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import NumBox from './textBox/NumBox';
+import TitleBox from './textBox/TitleBox';
+import DateBox from './textBox/DateBox';
+import AuthorBox from './textBox/AuthorBox';
+import DeadlineBox from './textBox/DeadlineBox';
 
 const Minutes = styled.li`
 	list-style: none;
@@ -9,8 +14,25 @@ const Minutes = styled.li`
 	height: 50px;
 `;
 
-function MinutesItem({ title }) {
-	return <Minutes>| {title}</Minutes>;
+function MinutesItem({ title, date, deadline }) {
+	const today = new Date().getDate();
+	const Dday =
+		deadline.substr(8, 2) - today < 0
+			? '종료'
+			: `D-${deadline.substr(8, 2) - today}`;
+
+	return (
+		<Minutes>
+			<NumBox>0</NumBox>
+			<TitleBox>{title}</TitleBox>
+			<DateBox>
+				{date.substr(2, 2)}/{date.substr(5, 2)}/{date.substr(8, 2)}{' '}
+				{date.substr(11, 5)}
+			</DateBox>
+			<AuthorBox>작성자</AuthorBox>
+			<DeadlineBox>{Dday}</DeadlineBox>
+		</Minutes>
+	);
 }
 
 export default MinutesItem;
