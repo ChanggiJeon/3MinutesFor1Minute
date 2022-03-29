986d5843346de9153950bdadf67df840ba969173
전창기 <wjs1724@naver.com>
2022-03-29 13:28:03 +0900

    Feat/AI/Chage google-stt-key
    Former-commit-id: e224fd2b63faaadc371e51ac5347325e9d1bcf37

--- BE/api/AI/STT/API/google.py
+++ BE/api/AI/STT/API/google.py
@@ -2,23 +2,23 @@ from google.cloud import storage
 from hanspell import spell_checker
 import re
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/multicampus/Desktop/S06P22D202/BE/api/AI/STT/API/ssafy-345204-8ee8c7463811.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/multicampus/Desktop/S06P22D202/BE/api/AI/STT/API/festive-vim-345604-8faec2e2d112.json'
 
 
 def upload_file(file_path, file_name):
     storage_client = storage.Client()
     buckets = list(storage_client.list_buckets())
-    bucket = storage_client.get_bucket("ssafy_data")
+    bucket = storage_client.get_bucket("glassix")
     blob = bucket.blob("audio/" + file_name)
     blob.upload_from_filename(file_path + file_name)
-    print(buckets)
+    print("success")
 
 
 def transcribe_gcs(file_name):
     from google.cloud import speech
 
 
-    gcs_uri = "gs://ssafy_data/audio/" + file_name
+    gcs_uri = "gs://glassix/audio/" + file_name
     client = speech.SpeechClient()
     audio = speech.RecognitionAudio(uri=gcs_uri)
     config = speech.RecognitionConfig(
@@ -60,7 +60,7 @@ def transcribe_gcs(file_name):
 
 
 # if __name__ == "__main__":
-#     upload_file("C:/Users/multicampus/Desktop/S06P22D202/BE/api/media/test/", "article.wav")
 
-text = transcribe_gcs("article.wav")
-print(text)
+upload_file("C:/Users/multicampus/Desktop/S06P22D202/BE/api/", "media/test/article.wav")
+# text = transcribe_gcs("article.wav")
+# print(text)
