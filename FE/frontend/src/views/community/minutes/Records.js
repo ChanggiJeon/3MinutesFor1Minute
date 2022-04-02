e3948e908fe34c4c5ae0d92569fd862f5b066ab8
John Kim <dhkdhd123@naver.com>
2022-04-02 12:01:14 +0900

    Feat/FE ; to save status
    Former-commit-id: beaa5d3731e26426b5f5dc79f131a9a519d8c3a4

--- FE/frontend/src/views/community/minutes/Records.js
+++ FE/frontend/src/views/community/minutes/Records.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Recorder } from 'react-voice-recorder';
 import Main from '../../../components/community/MainCenter';
 import GrayLgButton from '../../../components/common/GrayLgButton';
 import RecorderImg from '../../../components/community/minutes/speech/RecorderImg';
@@ -38,6 +37,7 @@ const RecContainer = styled(Container)`
 function Records() {
 	const navigate = useNavigate();
 	const { communityId, minutesId } = useParams();
+	const { recorderState, ...handlers } = Recorder();
 
 	const speechFinished = formData => {
 		console.log('formData', formData);
@@ -58,94 +58,31 @@ function Records() {
 	// 	const res = speechFinished(formData);
 	// 	console.log('res', res);
 	// }
-	const [speech, setSpeech] = useState({
-		audioDetails: {
-			url: null,
-			blob: null,
-			chunks: null,
-			duration: {
-				h: 0,
-				m: 0,
-				s: 0,
-			},
-		},
-	});
-	function handleAudioStop(data) {
-		console.log('stopped', data);
-		setSpeech({ audioDetails: data });
-	}
-
-	function handleAudioUpload(file) {
-		console.log('upload', file);
-		const fileName = `${new Date().toString()}.wav`;
-		const recordFile = new File([file], fileName);
-		console.log('recordFile', recordFile);
-		const formData = new FormData();
-		formData.append('record_file', recordFile);
-		formData.append('enctype', 'multipart/form-data');
-		formData.append('title', 'title');
-		const res = speechFinished(formData);
-		console.log('res', res);
-	}
-
-	function handleCountDown(data) {
-		console.log('countdown', data);
-	}
-
-	function handleReset() {
-		const reset = {
-			url: null,
-			blob: null,
-			chunks: null,
-			duration: {
-				h: 0,
-				m: 0,
-				s: 0,
-			},
-		};
-		setSpeech({ audioDetails: reset });
-	}
+	const [recordStatus, setRecordStatus] = useState('idle');
 
 	return (
 		<Main>
 			<RecContainer>
 				<TextSubTitle>스피치 생성(녹음)</TextSubTitle>
 				<DivLine />
-				{/* <MainBox>
+				<MainBox>
 					<SubBox>
-						<RecordsWave status={status} />
+						<RecordsWave status={recordStatus} />
 						<RecorderImg src={recorder} />
 					</SubBox>
 					<SubBox>
-						<Timer status={} />
+						<Timer status={recordStatus} />
 					</SubBox>
-				</MainBox> */}
-				<Recorder
-					style={{ width: '500px' }}
-					record={false}
-					hideHeader
-					handleAudioStop={data => handleAudioStop(data)}
-					handleAudioUpload={data => handleAudioUpload(data)}
-					handleReset={() => handleReset()}
-					mimeTypeToUseWhenRecording='audio/webm' // For specific mimetype.
-				/>
-				{/* <BtnBox>
-					<audio controls src={mediaBlobUrl}>
+				</MainBox>
+				<BtnBox>
+					<audio controls src=''>
 						<track kind='captions' />
 					</audio>
-					<StartBtn status={status} onClick={startRecording}>
-						스피치 시작
-					</StartBtn>
-					<PauseBtn status={status} onClick={pauseRecording}>
-						일시 정지
-					</PauseBtn>
-					<ResumeBtn status={status} onClick={resumeRecording}>
-						다시 시작
-					</ResumeBtn>
-					<StopBtn status={status} onClick={stopRecording}>
-						스피치 종료
-					</StopBtn>
-				</BtnBox> */}
+					<StartBtn status={recordStatus}>스피치 시작</StartBtn>
+					<PauseBtn status={recordStatus}>일시 정지</PauseBtn>
+					<ResumeBtn status={recordStatus}>다시 시작</ResumeBtn>
+					<StopBtn status={recordStatus}>스피치 종료</StopBtn>
+				</BtnBox>
 			</RecContainer>
 		</Main>
 	);
