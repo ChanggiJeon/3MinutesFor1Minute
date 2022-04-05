bf86fbddaf9df318dfeea1a5d65b74654a9213df
윤재성 <wotjd4725@naver.com>
2022-04-05 08:30:10 +0000

    Update views.py
    Former-commit-id: 2a094eeb85486de90ea5d4cb2a0f4d41ba2b0cfb

--- BE/api/boards/views.py
+++ BE/api/boards/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
-from pyrsistent import b
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
