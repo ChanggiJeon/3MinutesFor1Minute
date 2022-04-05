48edcf5d6d230868de9d36b767f71faf663adb90
John Kim <dhkdhd123@naver.com>
2022-04-05 09:47:06 +0900

    Feat/FE/Add : speechCreate
    Former-commit-id: 1c0736854e0ef82db04c836d7fef55b33f331e1b

--- FE/frontend/src/api/speech.js
+++ FE/frontend/src/api/speech.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
 import { BASE_URL, setToken } from './utils';
 
-export default async function createSpeech(comId, minId, data) {
+export async function createSpeech(comId, minId, data) {
 	try {
 		const response = await axios({
 			method: 'post',
 			url: `${BASE_URL}/${comId}/minutes/${minId}/speech/create/`,
+			data,
+			headers: {
+				...setToken(),
+				'Content-Type': 'multipart/form-data',
+			},
+		});
+		return response.data;
+	} catch (err) {
+		console.log(err);
+		return err.response.data;
+	}
+}
+export async function updateSpeech(comId, minId, spcId, data) {
+	try {
+		const response = await axios({
+			method: 'post',
+			url: `${BASE_URL}/${comId}/minutes/${minId}/speech/${spcId}/update/`,
 			data,
 			headers: {
 				...setToken(),
