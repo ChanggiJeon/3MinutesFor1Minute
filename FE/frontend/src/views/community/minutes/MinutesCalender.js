30aecfe461a8b450fb097d0362ab3a126e4b19cc
전창기 <wjs1724@naver.com>
2022-04-06 15:15:14 +0900

    Feat/FE/Add : MinutesCalender with some errors
    Former-commit-id: 477e5b85e106c6b533a16a35b2ad8ba0816e7f36

--- FE/frontend/src/views/community/minutes/MinutesCalender.js
+++ FE/frontend/src/views/community/minutes/MinutesCalender.js
@@ -1,23 +1,59 @@
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { Scheduler } from '@aldabil/react-scheduler';
+import styled from 'styled-components';
+import dayjs from 'dayjs';
+import routes from '../../../routes';
+import Container from '../../../components/community/Container';
+import Main from '../../../components/community/MainCenter';
+import TextSubTitle from '../../../components/common/TextSubTitle';
+import DivLine from '../../../components/community/main/DivLine';
+import MainBox from '../../../components/community/minutes/list/MainBox';
+import BlueMdBtn from '../../../components/common/BlueMdBtn';
+import { fetchMinutesByComId } from '../../../store/minutes';
+import HeaderBox from '../../../components/community/HeaderBox';
+import MinutesPagination from './MinutesPagination';
+
+import MinutesItem from '../../../components/community/minutes/list/MinutesItem';
+import NumBox from '../../../components/community/minutes/list/textBox/NumBox';
+import TitleBox from '../../../components/community/minutes/list/textBox/TitleBox';
+import DateBox from '../../../components/community/minutes/list/textBox/DateBox';
+import AuthorBox from '../../../components/community/minutes/list/textBox/AuthorBox';
+import DeadlineBox from '../../../components/community/minutes/list/textBox/DeadlineBox';
 
 function MinutesCalender() {
+	const { communityId } = useParams();
+	const dispatch = useDispatch();
+	const now = dayjs()
+	const [events, setEvents] = useState([]);
+	const location = useLocation();
+
+	useEffect(() => {
+		dispatch(fetchMinutesByComId(communityId)).then(({payload})=>{
+			const events = [];
+			payload.map(event => {
+				const a = {
+					event_id: event.id,
+					title: event.title,
+					start: new Date(event.created_at),
+					end: new Date(event.deadline),
+				}
+				events.push(a)
+				return event
+			});
+			console.log(events);
+			setEvents([events]);
+		});
+	}, [location]);
+	
 	return (
 		<Scheduler
-			view='month'
-			events={[
-				{
-					event_id: 1,
-					title: 'Event 1',
-					start: new Date('2021/5/2 09:30'),
-					end: new Date('2021/5/2 10:30'),
-				},
-				{
-					event_id: 2,
-					title: 'Event 2',
-					start: new Date('2021/5/4 10:00'),
-					end: new Date('2021/5/4 11:00'),
-				},
-			]}
+			view='week'
+			events={
+				events
+			}
+			selectedDate={new Date(now)}
 		/>
 	);
 }
