bdbe880c933561b62025821e020e36b4fa19fdce
kiel <aceg1541@naver.com>
2022-04-07 01:04:54 +0900

    Fix/BE : Thread
    Former-commit-id: 7815344494fc9c927d8057c40368287339d6ffe5

--- BE/api/Thread.py
+++ BE/api/Thread.py
@@ -3,8 +3,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 import django
 django.setup()
-from django.shortcuts import get_list_or_404, get_object_or_404
-from minutes.models import Minute
+from django.shortcuts import get_list_or_404
+from minutes.models import Minute, Participant
 from notifications.models import Notification
 import concurrent.futures
 
@@ -14,21 +14,32 @@ def activate_notification():
         notifications = get_list_or_404(Notification, is_activate=False)
 
         for notification in notifications:
-            diff = notification.minute.deadline - datetime.datetime.now()
-
-            if diff.seconds <= 0:
-                minute = get_object_or_404(Minute, pk=notification.minute.pk)
-                minute.is_closed = True
-                minute.save()
-
-            elif diff.seconds <= 3600:
+            if (notification.minute.deadline - datetime.datetime.now()).seconds <= 3600:
                 notification.is_activate = True
                 notification.save()
+
+        minutes = get_list_or_404(Minute, is_closed=False)
+
+        for minute in minutes:
+            if (minute.deadline - datetime.datetime.now()).second <= 0:
+                minute.is_closed = True
+                minute.save()
+                participants = get_list_or_404(Participant, minute=minute)
+
+                for participant in participants:
+                    notification = Notification(
+                        user=participant.member.user,
+                        minute=minute,
+                        content=f'{minute.title}이 마감되었습니다.',
+                        is_activate=True
+                    )
+
+                    notification.save()
 
     except:
         print("there is no notification such conditions")
 
-    time.sleep(50)
+    time.sleep(60)
 
 
 if __name__ == "__main__":
