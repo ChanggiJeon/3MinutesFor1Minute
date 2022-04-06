c043e4e5c0cf1c948f2b6bc1a21b63b7a0df5b84
전창기 <wjs1724@naver.com>
2022-04-06 16:52:17 +0900

    Feat/FE/Add : Calender function
    Former-commit-id: 9563640f80763204384790306054e11b91ecc064

--- FE/frontend/src/views/community/minutes/MinutesCalender.js
+++ FE/frontend/src/views/community/minutes/MinutesCalender.js
@@ -1,30 +1,40 @@
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { Scheduler } from '@aldabil/react-scheduler';
-import styled from 'styled-components';
 import dayjs from 'dayjs';
-import routes from '../../../routes';
-import Container from '../../../components/community/Container';
-import Main from '../../../components/community/MainCenter';
-import TextSubTitle from '../../../components/common/TextSubTitle';
-import DivLine from '../../../components/community/main/DivLine';
-import MainBox from '../../../components/community/minutes/list/MainBox';
-import BlueMdBtn from '../../../components/common/BlueMdBtn';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import styled from "@emotion/styled";
 import { fetchMinutesByComId } from '../../../store/minutes';
-import HeaderBox from '../../../components/community/HeaderBox';
-import MinutesPagination from './MinutesPagination';
 
-import MinutesItem from '../../../components/community/minutes/list/MinutesItem';
-import NumBox from '../../../components/community/minutes/list/textBox/NumBox';
-import TitleBox from '../../../components/community/minutes/list/textBox/TitleBox';
-import DateBox from '../../../components/community/minutes/list/textBox/DateBox';
-import AuthorBox from '../../../components/community/minutes/list/textBox/AuthorBox';
-import DeadlineBox from '../../../components/community/minutes/list/textBox/DeadlineBox';
+
+export const StyleWrapper = styled.div`
+  .fc {
+	margin : 20px
+  }
+  .fc-event-main-frame .fc-daygrid-dot-event{
+	flex-direction: row-reverse;
+  }
+  .fc-daygrid-event-dot{
+	display:none;
+  }
+  .fc-event-time{
+	display:none;
+  }
+  .fc-event-title{
+	text-align: center;
+	font-size: 15px
+  }
+  .fc-event-title-container{
+	text-align: center;
+  }
+
+`
 
 function MinutesCalender() {
 	const { communityId } = useParams();
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const now = dayjs()
 	const [events, setEvents] = useState([]);
 	const location = useLocation();
@@ -34,27 +44,35 @@ function MinutesCalender() {
 			const events = [];
 			payload.map(event => {
 				const a = {
-					event_id: event.id,
+					id: event.id,
 					title: event.title,
-					start: new Date(event.created_at),
-					end: new Date(event.deadline),
+					start: event.created_at,
+					end: event.deadline,
 				}
 				events.push(a)
 				return event
 			});
 			console.log(events);
-			setEvents([events]);
+			setEvents(events);
 		});
 	}, [location]);
 	
 	return (
-		<Scheduler
-			view='week'
-			events={
-				events
-			}
-			selectedDate={new Date(now)}
-		/>
+		<div className="App">
+			<StyleWrapper>
+				<FullCalendar
+					plugins={[dayGridPlugin]}
+					initialView="dayGridMonth"
+					headerToolbar={{
+						center: 'dayGridMonth',
+					}}
+					events={events}
+					eventColor='#537791'
+					nowIndicator
+					eventClick={(e) => navigate(`/community/${communityId}/minutes/${e.event._def.publicId}`)}
+				/>
+			</StyleWrapper>
+		</div>
 	);
 }
 
